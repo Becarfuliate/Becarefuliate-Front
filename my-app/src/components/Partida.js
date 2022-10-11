3b2c3d29a29e8ad308674619aceef361c947a862
AgustinCapogrossi <agus_capo_19@hotmail.com>
2022-10-11 12:54:07 -0300

    Formulario completo y contrasenia

--- my-app/src/components/Partida.js
+++ my-app/src/components/Partida.js
@@ -1,54 +1,75 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 /**
- * 
+ *
  * Usada para crear una partida con parámetros modificables
  */
-function crearPartida () {
-
-  const [state, setState] = useState ({      
-    nombre: '',
-    max_players: '',
-    max_rondas: ''
+function CrearPartida() {
+  const [state, setState] = useState({
+    nombre: "",
+    max_players: "",
+    max_rondas: "",
+    max_juegos: "",
+    contrasenia: "",
   });
-    
+
   const handleChange = (evt) => {
     // check it out: we get the evt.target.name (which will be either "nombre" or "Maximo de jugadores")
     // and use it to target the key on our `state` object with the same name, using bracket syntax
-    setState({...state, [evt.target.name]: evt.target.value });
+    setState({ ...state, [evt.target.name]: evt.target.value });
   };
   const handleSubmit = (event) => {
-    console.log('A name was submitted: ' + state.nombre);
-    console.log('A max player was submitted: ' + state.max_players);
-    console.log('A max player was submitted: ' + state.max_rondas);
+    console.log("A name was submitted: " + state.nombre);
+    console.log("A max player was submitted: " + state.max_players);
+    console.log("A max round was submitted: " + state.max_rondas);
+    console.log("An ammount of games was submitted: " + state.max_juegos);
     event.preventDefault();
   };
-    return (
-      <form onSubmit={handleSubmit}>
-      
-        <label>Nombre de partida</label>
-        <input type="text" name="nombre" onChange={handleChange} required/>
-        
-        <label>Maximo de jugadores</label>
-        <input type="number" name="max_players" onChange={handleChange} required/>
+  const [lectura, setLectura] = useState(true);
 
-        <label>Maximo de rondas</label>
-        <input type="number" name="max_rondas" onChange={handleChange} required/>
+  const changeState = () => {
+    if (lectura === false) setLectura(true);
+    else setLectura(false);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Nombre de partida</label>
+      <input type="text" name="nombre" onChange={handleChange} required />
 
-        <input type="submit" name="Submit" />
-      </form>
-    );  
-  
+      <label>Maximo de jugadores</label>
+      <input
+        type="number"
+        name="max_players"
+        onChange={handleChange}
+        required
+      />
+
+      <label>Maximo de rondas</label>
+      <input type="number" name="max_rondas" onChange={handleChange} required />
+
+      <label>Cantidad de juegos</label>
+      <input type="number" name="max_juegos" onChange={handleChange} required />
+      <label>Contraseña</label>
+      <button type="button" onClick={changeState}>
+        Click me!
+      </button>
+      <input
+        type="text"
+        name="contrasenia"
+        onChange={handleChange}
+        readOnly={lectura}
+      />
+      <input type="submit" name="Submit" />
+    </form>
+  );
 }
 
-function Partida () {
-    return (
-        <div>
-          <crearPartida/>
-        </div>
-    );
+function Partida() {
+  return (
+    <div>
+      <CrearPartida />
+    </div>
+  );
 }
-
 
 export default Partida;
