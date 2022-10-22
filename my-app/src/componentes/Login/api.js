e0677f0f8d6374d0cad986553b93a9c9e89d3216
ivan <ivanpereyra6654@gmail.com>
2022-10-22 17:23:05 -0300

    PB-2 fix, quitado de token pasado por header por innecesario se pasa token por url o body

--- my-app/src/componentes/Login/api.js
+++ my-app/src/componentes/Login/api.js
@@ -1,19 +1,10 @@
 import axios from "axios";
 
-const authHeader = () => {
-    const user = JSON.parse(localStorage.getItem('user'));
-
-    if(user && user.token) {
-        return { Authorization: 'Bearer' + user.token}
-    } else {
-        return {};
-    }
-};
+// token enviado por body o params por lo tanto innecesario enviarlo por headers
 
 const URL = 'http://127.0.0.1:8000/'; // aca seria la url del back
 
 export default axios.create({
         baseURL: URL,
         timeout: 0,
-        headers: authHeader()
 });
