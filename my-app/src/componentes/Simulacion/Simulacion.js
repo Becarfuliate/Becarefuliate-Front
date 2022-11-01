33432387a2d82e88739a027557e6853d5a595a40
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2022-11-01 11:09:13 -0300

    ANIMACION ANDANDO WIII

--- my-app/src/componentes/Simulacion/Simulacion.js
+++ my-app/src/componentes/Simulacion/Simulacion.js
@@ -13,17 +13,17 @@ function Simulacion() {
 
   // Robot ID - IMG - Coordendas x;y - nombre
   ronda0.push({id: 0, imagen: "https://opengameart.org/sites/default/files/styles/medium/public/robot-preview.png", x: 500, y: 1000, xf: 700, yf: 1000, nombre: 'Joe', vida: 45, mira: 360, motor: 0});
-  ronda0.push({id: 1, imagen: "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png", x: 0, y: 0, xf: 0, yf: 0, nombre: 'Mati', vida: 100, mira: 360, motor: 0});
+  ronda0.push({id: 1, imagen: "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png", x: 0, y: 0, xf: 100, yf: 100, nombre: 'Mati', vida: 100, mira: 360, motor: 0});
   ronda0.push({id: 2, imagen: "https://toppng.com/uploads/preview/8-bit-mario-8-bit-luigi-pixel-115633511986zud7ifcqi.png", x: 450, y: 450, xf: 550, yf: 450, nombre: 'Mario', vida: 96, mira: 45, motor: 0});
   ronda0.push({id: 3, imagen: "https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png", x: 0, y: 0, xf: 500, yf: 500, nombre: 'Steve', vida: 100, mira: 360, motor: 0});
   
   ronda1.push({id: 0, imagen: "https://opengameart.org/sites/default/files/styles/medium/public/robot-preview.png", x: 700, y: 1000, xf: 900, yf: 1000, nombre: 'Joe', vida: 45, mira: 360, motor: 0});
-  ronda1.push({id: 1, imagen: "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png", x: 490, y: 0, xf: 490, yf: 0, nombre: 'Mati', vida: 100, mira: 360, motor: 0});
+  ronda1.push({id: 1, imagen: "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png", x: 100, y: 100, xf: 200, yf: 200, nombre: 'Mati', vida: 100, mira: 360, motor: 0});
   ronda1.push({id: 2, imagen: "https://toppng.com/uploads/preview/8-bit-mario-8-bit-luigi-pixel-115633511986zud7ifcqi.png", x: 550, y: 450, xf: 650, yf: 450, nombre: 'Mario', vida: 96, mira: 45, motor: 0});
   ronda1.push({id: 3, imagen: "https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png", x: 500, y: 500, xf: 1000, yf: 1000, nombre: 'Steve', vida: 100, mira: 360, motor: 0});
   
   ronda2.push({id: 0, imagen: "https://opengameart.org/sites/default/files/styles/medium/public/robot-preview.png", x: 900, y: 1000, xf: 1000, yf: 900, nombre: 'Joe', vida: 45, mira: 360, motor: 0});
-  ronda2.push({id: 1, imagen: "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png", x: 1000, y: 0, xf: 1000, yf: 0, nombre: 'Mati', vida: 100, mira: 360, motor: 0});
+  ronda2.push({id: 1, imagen: "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png", x: 200, y: 200, xf: 300, yf: 300, nombre: 'Mati', vida: 100, mira: 360, motor: 0});
   ronda2.push({id: 2, imagen: "https://toppng.com/uploads/preview/8-bit-mario-8-bit-luigi-pixel-115633511986zud7ifcqi.png", x: 650, y: 450, xf: 700, yf: 450, nombre: 'Mario', vida: 96, mira: 45, motor: 0});
   ronda2.push({id: 3, imagen: "https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png", x: 1000, y: 1000, xf: 1000, yf: 1000, nombre: 'Steve', vida: 100, mira: 360, motor: 0});
   
