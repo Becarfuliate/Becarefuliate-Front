11736589196e1ed00069e7320f199c418cc8f7f9
Alexiuus <Alexis.centeno@mi.unc.edu.ar>
2022-11-16 12:54:53 -0300

    quitando botón de password y aplicando redux

--- my-app/src/store/Partidas/actions.js
+++ my-app/src/store/Partidas/actions.js
@@ -5,4 +5,48 @@ const getDataGamesUser = (callback) => {
     }
 };
 
-export {getDataGamesUser};
+const sendDataGame = () => {
+    return {
+        type: "SEND_DATA_GAME",
+        data: "send the user data to register"
+    }
+};
+
+const modifyDataNameGame = (name) => {
+    return {
+        type: "MODIFY_DATA_NAME_GAME",
+        data: name
+    }
+};
+
+const modifyDataPasswordGame = (password) => {
+    return {
+        type: "MODIFY_DATA_PASS_GAME",
+        data: password
+    }
+};
+
+const modifyDataRoundsGame = (rounds) => {
+    return {
+        type: "MODIFY_DATA_ROUNDS_GAME",
+        data: parseInt(rounds, 32)
+    }
+};
+
+const modifyDataMaxPlayersGame = (players) => {
+    return {
+        type: "MODIFY_DATA_MAX_PLAYERS_GAME",
+        data: parseInt(players, 32)
+    }
+};
+
+const modifyDataGamesGame = (game) => {
+    return {
+        type: "MODIFY_DATA_GAME_GAME",
+        data: parseInt(game, 32)
+    }
+};
+
+export {getDataGamesUser, sendDataGame, modifyDataNameGame, 
+        modifyDataPasswordGame, modifyDataRoundsGame, modifyDataGamesGame,
+        modifyDataMaxPlayersGame};
--- my-app/src/store/Partidas/reducer.js
+++ my-app/src/store/Partidas/reducer.js
@@ -1,13 +1,33 @@
-import {getToken, servicioListarGames} from './service';
+import {getToken, servicioListarGames, getNameUser, servicioPartida} from './service';
 
 const defaultDataUser = {
+    name: "",
+    max_players: 0,
+    min_players: 2,
+    password: "",
+    n_matchs: 0,
+    n_rounds_matchs: 0,
+    user_creator: getNameUser(),
     token: getToken()
 };
 
-function reducer(dataUser = defaultDataUser, action){   
-    if (action.type === 'GET_DATA_GAMES_USER') return servicioListarGames(dataUser.token, action.data);
+function reducer(dataGame = defaultDataUser, action){ 
+    const dataGameChange = (atrib, data) => {
+        return {
+            ...dataGame,
+            [atrib]: data
+        }
+    };
     
-    return dataUser;
+    if (action.type === 'GET_DATA_GAMES_USER') servicioListarGames(dataGame.token, action.data);
+    else if (action.type === 'SEND_DATA_GAME') servicioPartida(dataGame, action.data); 
+    else if (action.type === 'MODIFY_DATA_NAME_GAME') return dataGameChange("name", action.data);
+    else if (action.type === 'MODIFY_DATA_MAX_PLAYERS_GAME') return dataGameChange("max_players", action.data); 
+    else if (action.type === 'MODIFY_DATA_PASS_GAME') return dataGameChange("password", action.data); 
+    else if (action.type === 'MODIFY_DATA_GAME_GAME') return dataGameChange("n_matchs", action.data); 
+    else if (action.type === 'MODIFY_DATA_ROUNDS_GAME') return dataGameChange("n_rounds_matchs", action.data);
+ 
+    return dataGame;
 }
 
 export default reducer;
