f3b8e4e029e9d5fd9b2d418e7c3b77be732c2979
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2022-11-17 18:00:22 -0300

    Reducción del tamaño del Tablero de Simulación

--- my-app/src/componentes/Simulacion/Simulacion.js
+++ my-app/src/componentes/Simulacion/Simulacion.js
@@ -3,7 +3,6 @@ import Tablero from './Tablero';
 function Simulacion() {
   const rondas = JSON.parse(localStorage.getItem("simulacion"));
 
-  
   return (
     <div id="Simulacion">
       <Tablero rondas={rondas}/>
