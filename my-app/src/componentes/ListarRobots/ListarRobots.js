66614b06c47b41f73b54e33695bf62340b8cad1a
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2022-11-21 17:20:29 -0300

    ListarRobots funcional

--- my-app/src/componentes/ListarRobots/ListarRobots.js
+++ my-app/src/componentes/ListarRobots/ListarRobots.js
@@ -2,8 +2,9 @@ import {getDataRobotsUser} from '../../store/robots/actions';
 import React, {useEffect, useState} from "react";
 import {connect} from 'react-redux';
 import './ListarRobots.css';
+import axios from 'axios';
 
-const defaultimgRobot = "./robot.png"
+// const defaultimgRobot = "./robot.png"
 
 function ListarRobots({getDataRobotsUser}){
     const [listRobots, setListRobots] = useState([]);
@@ -14,18 +15,34 @@ function ListarRobots({getDataRobotsUser}){
         else getDataRobotsUser(useResponseDataRobot);
     }, [responseDataRobot, getDataRobotsUser]);
 
-     return (<Listing robots={listRobots}/>);
+    return (<Listing robots={listRobots}/>);
 };
 
 function Listing(listRobots) {
 
     const listResults = [];
 
+    const [RobotImages, setRobotImages] = useState([]);
+    useEffect(() => {
+        const obtenerImagen = async (robotId) => {
+            const tkn = JSON.parse(localStorage.getItem("user")).token;
+            const baseURL = "http://127.0.0.1:8000";
+            const response = await axios.get(baseURL + "/image", {params: { token: tkn, robot_id: robotId }});
+            
+            setRobotImages(RobotImages => [...RobotImages, response.data]);
+        }
+        // eslint-disable-next-line
+        listRobots.robots.map( robot => {
+            obtenerImagen(robot.id);
+        })
+    }, [listRobots])
+    
+
     // eslint-disable-next-line
     listRobots.robots.map((robot, index) => {
         listResults.push(
             <div className='block-robot' key={index} data-testid="robot-name">
-                <img src={defaultimgRobot} alt="" />
+                <img src={`data:image/png;base64,${RobotImages[index]}`} alt="" />
                 <p className="RobotName"> Nombre: {robot.name} </p>
                 <p className="RobotId"> ID: {robot.id} </p>
                 <p> Partidas Jugadas: {robot.matchs_pleyed} </p>
@@ -40,3 +57,6 @@ function Listing(listRobots) {
 };
 
 export default connect(null, {getDataRobotsUser})(ListarRobots);
+
+
+    
