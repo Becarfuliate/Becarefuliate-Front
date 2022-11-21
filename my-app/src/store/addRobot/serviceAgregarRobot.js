5ae0723ad5b28674469a383439789b3fecd2e698
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2022-11-21 15:59:42 -0300

    Cambios, miras a arreglo

--- my-app/src/store/addRobot/serviceAgregarRobot.js
+++ my-app/src/store/addRobot/serviceAgregarRobot.js
@@ -1,14 +1,26 @@
 import exportServiceLogin from '../../componentes/Servicios/serviceLogin';
 import verifyDataRobot from './verifyDataRobot';
 import axios from 'axios';
+import swal from 'sweetalert';
 
 const baseURL = 'http://localhost:8000';
 const endpoint = '/upload/robot';
 
 const handleResponse = (code, respuesta) => {
-    if(code === 200) alert(respuesta.msg);
-    else alert("Error en los datos");
-    
+    if(code === 200) {
+        swal({
+            text: respuesta.msg,
+            icon: 'success',
+            timer: '1800'
+        });
+    }
+    else {
+        swal({
+            text: 'Error en los datos.',
+            icon: 'error',
+            timer: '1800'
+        });
+    }
     if(code === 400 || code === 404) exportServiceLogin.serviceLogOut();
 
 };
@@ -17,18 +29,16 @@ const filesRobot = (dataRobot) => {
     const files = new FormData();
     files.append('config', dataRobot.config);
     files.append('avatar', dataRobot.avatar);
-    return files;
+    return {body: files};
 };
 
 const paramsData = (name) => {
     const user = JSON.parse(localStorage.getItem('user'));
     const token = (user)? user.token : '';
-    const userLogin = (user)? user.userlogin : '';
 
     return {
-            name: name,
-            tkn: token,
-            username: userLogin
+        name: name,
+        tkn: token
     }
 };
 
@@ -41,12 +51,20 @@ const headers = () => {
 
 const serviceUploadRobot = async (dataRobot) => {
     const resultsVerifyDataRobot = verifyDataRobot(dataRobot);
-    if (resultsVerifyDataRobot.state === 'OK')
+    if (resultsVerifyDataRobot.state === 'OK') {
+        console.log(paramsData(dataRobot.name));
+        console.log(filesRobot(dataRobot));
         await axios.post(baseURL + endpoint, paramsData(dataRobot.name), filesRobot(dataRobot), headers())
         .then(respuesta => handleResponse(respuesta.status, respuesta.data))
         .catch((error) => handleResponse(error.response.status, error.response.data));
-    else
-        alert(resultsVerifyDataRobot.data);
+    }
+    else {
+        swal({
+            text: resultsVerifyDataRobot.data,
+            icon: 'error',
+            timer: '2500'
+        });
+    }
 };
 
 const exportServiceRobot = {
